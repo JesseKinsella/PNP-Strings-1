01ebc3565aaefd148f9573e65551cd1a48590405
JesseKinsella1 <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 14:39:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+age = 37
+weight = 165.5
+
+shirt = "black" #strings need ""
+
+print(shirt)
+
+store = "Jesse's Pizza Shop" # why single quotes aren't great for string
+
+print(store)
+
+store2 = 'Jesse\'s Piza Shop, the "best" there is' 
+# but need a backslash if you dont want to treat something as the end of a string
+print(store2)
+
+movie = 'The King\'s Man'
+movie_alt = "The King's Man"
+
+print(movie)
+print(movie_alt)
